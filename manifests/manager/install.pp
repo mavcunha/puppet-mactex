71800f61965a065d5c1e92bf92ddaf93a3efaf9a
James Sweet <james.sweet88@googlemail.com>
2013-04-02 14:11:38 -0400

    Corrected install script

--- manifests/manager/install.pp
+++ manifests/manager/install.pp
@@ -1,8 +1,8 @@
 define mactex::manager::install {
-	exec { "Installing Package: ${package}":
+	exec { "Installing Package: ${name}":
 		path   => "/usr/bin:/usr/sbin:/bin",
-		command => "/usr/texbin/tlmgr install ${package}",
-		unless => "/usr/texbin/tlmgr info achemso | grep installed: | cut -d' ' -f4 | awk '{ exit \$0 != \"Yes\" }'",
+		command => "/usr/texbin/tlmgr install ${name}",
+		unless => "/usr/texbin/tlmgr info ${name} | grep installed: | cut -d' ' -f4 | awk '{ exit \$0 != \"Yes\" }'",
 		user => "root"
 	}
 }
