dbb3a47f75aaf9cbd274a172dea00a3fbda67873
James Sweet <james.sweet88@googlemail.com>
2013-04-02 14:00:29 -0400

    Changed install to be a define and added a test to only execute if it is not installed

--- manifests/manager/install.pp
+++ manifests/manager/install.pp
@@ -1,6 +1,14 @@
-class mactex::manager::install($package) {
+define mactex::manager::install(
+  $package   = undef,
+) {
+	if ($package == undef) {
+        fail('Cannot install empty packages')
+      }
+
 	exec { "Installing Package: ${package}":
+		path   => "/usr/bin:/usr/sbin:/bin",
 		command => "/usr/texbin/tlmgr install ${package}",
+		unless => "/usr/texbin/tlmgr info achemso | grep installed: | cut -d' ' -f4 | awk '{ exit \$0 != \"Yes\" }'",
 		user => "root"
 	}
 }
